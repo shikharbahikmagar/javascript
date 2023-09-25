ab47c609fe7615a3d9aa98600c77f14e28a19f5a
Shikhar bahik magar <38580729+shikharbahikmagar@users.noreply.github.com>
2023-09-25 06:58:10 +0000

    conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,16 +4,27 @@ let val = null;//conversion = 0
 let val1 = undefined;//conversion = nan
 
 let scoreInt = Number(score);
-console.log(typeof scoreInt);
-console.log(scoreInt); //33
+//console.log(typeof scoreInt);
+//console.log(scoreInt); //33
 
 let number_emp_id = Number(emp_id);
-console.log(typeof number_emp_id);
+//console.log(typeof number_emp_id);
 
-console.log(number_emp_id); //NAN
+//console.log(number_emp_id); //NAN
 
-console.log(val);
+//console.log(val);
 
 //true = 1, false = 0
 // "" => false "text" => true
 //conversion = String(), Number(), Boolean() etc
+
+
+//******** operations *********** */
+//oper(2**3) 2 ko power ma 3
+
+let value =3
+let negValue = -value
+//console.log(negValue);
+//console.log("1" + 2 + 2); //122
+//console.log(1 + 2 + "2"); //32
+//console.log(+true); //give 1 due to + sign "true = 1"
