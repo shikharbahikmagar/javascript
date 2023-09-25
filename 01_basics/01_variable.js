6186737efe63762ac5459a8ea325ce19054d898f
Shikhar bahik magar <38580729+shikharbahikmagar@users.noreply.github.com>
2023-09-25 06:28:37 +0000

    variables and data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+//not replaceable
+const accountId = 2345;
+
+let accountEmail = "admin@admin.com" 
+var password = "admin@123"
+city ="pokhara"
+
+//console.log(accountId);
+console.table([accountId, accountEmail, password, city])
---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as new javascript
+
+//alert("hello"); we are using nodejs, not browser
+
+// number = 2 to power of 53
+//bigint
+//boolean = true or false
+//string = ""
+//null = standalone value
+//undefiend = 
+//symbol = unique
+//object
+
+let age = 22
+let isLoggedIn = true
+let state = null 
+
+console.log(typeof null) // output object
+console.log(typeof undefined); //output undefined
+
