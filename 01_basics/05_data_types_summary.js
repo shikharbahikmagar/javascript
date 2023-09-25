e498ae431ea087fd01ed142e69fce3e0865f90dd
shikharbahikmagar <shikharbahik5@gmail.com>
2023-09-25 17:23:45 +0545

    test

---
+++ 01_basics/05_data_types_summary.js
@@ -0,0 +1,29 @@
+// # <<<<<<< HEAD
+// # Return type of variables in JavaScript
+// # 1) Primitive Datatypes
+// #        Number => number
+// #        String  => string
+// #        Boolean  => boolean
+// #        null  => object
+// #        undefined  =>  undefined
+// #        Symbol  =>  symbol
+// #        BigInt  =>  bigint
+
+// # 2) Non-primitive Datatypes
+// #        Arrays  =>  object
+// #        Function  =>  function
+// # =======
+// # Return type of variables in JavaScript
+// # 1) Primitive Datatypes
+// #        Number => number
+// #        String  => string
+// #        Boolean  => boolean
+// #        null  => object
+// #        undefined  =>  undefined
+// #        Symbol  =>  symbol
+// #        BigInt  =>  bigint
+
+// # 2) Non-primitive Datatypes
+// #        Arrays  =>  object
+// #        Function  =>  function
+// #        Object  =>  object
