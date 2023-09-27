3a601b8d00eb509b04e75f248289cb36d8c06c31
shikharbahikmagar <shikharbahik5@gmail.com>
2023-09-27 18:45:06 +0545

    iife in javascript

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+//iife = Immediately Invoked Function Expression
+//type of function runs immediately after event occur
+
+//iife for normal function
+(function add()
+{
+    console.log(2+2);
+})();
+
+//for arrow function
+
+(() => {
+    console.log(3+2);
+})()
