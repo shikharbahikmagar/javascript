19de6babac9c1c03fb03d967e94cd74a8ac4194a
shikharbahikmagar <shikharbahik5@gmail.com>
2023-09-28 10:07:00 +0545

    while loop

---
+++ 05_iterations/while.js
@@ -0,0 +1,23 @@
+let index = 0
+
+while (index <= 10) 
+{
+    //console.log(`value of index ${index}`);
+    index += 2    
+}
+
+
+let myArray = [1, 2, 3, 4]
+let i = 0;
+while (i < myArray.length ) {
+    //console.log(myArray[i]);
+    i++;
+}
+
+let score = 1
+do{
+
+    console.log(`score is ${score}`);
+    score++;
+
+}while(score <= 10)
